ac1a9b2c4c369064b91a531b52939b724e4d9d03
dasuiyuanhao <dasuiyuanhao@gmail.com>
2020-09-26 11:55:30 +0800

    首次代码提交

---
+++ init.js
@@ -0,0 +1,9 @@
+
+window.patch = snabbdom.init([
+    snabbdom_class.default, snabbdom_style.default, snabbdom_props.default, snabbdom_eventlisteners.default
+]);
+window.h = snabbdom.h;
+
+window.all_metrics = {
+    tp50: 'Top 50 (ms)',
+};
---
+++ load.js
@@ -0,0 +1,35 @@
+
+
+function init(){
+    console.info("开始加载......")
+
+    return new Promise((resolve, reject)=>{
+        //找到页面元素
+        const left_doc = ()=>
+            document.getElementById('main');
+    
+        async.waterfall([
+            cb=>{
+                async.until(
+                    ()=> left_doc() != null
+                    , cb=>console.log('waiting for 页面加载') || setTimeout(cb, 1000)
+                    , ()=>{
+                        //left_doc().querySelector('#systemName3 a').click()
+
+                        cb(null, null)
+                    }
+                )
+            }, (app_list, cb)=>{
+                    //加载数据
+                    // loadData();
+                    console.info("加载成功")
+                }
+
+        ], (e, r)=>e ? reject(e) : resolve(r))
+    })
+}
+
+
+
+
+
