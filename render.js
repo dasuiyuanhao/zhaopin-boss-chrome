70967baeff2395a455e3c09bc7545fd4f9c34750
liuxiaoliang1 <liuxiaoliang@idstaff.com>
2020-11-18 18:38:01 +0800

    BOSS直聘页面改版

--- render.js
+++ render.js
@@ -1,25 +1,25 @@
 //抓取页面的锚点
 var $main=null;
-var windowIframeName="syncFrame";
+var windowIframeName="recommendFrame";
 
 const panel = document.createElement('div');
 panel.id = 'extend_panel'
 panel.innerHTML = `
 <div class="left">
-    <h3 style="color:black;background-color: lightblue;">控制台</h3>
+    <h3 style="color:black;background-color: lightblue;padding-left: 10px;">控制台</h3>
     
     
     <div class="dashboard-edit-box" style="line-height: 30px;">
         <span>打开【推荐牛人】页面，先滚动到页面底部加载简历(也可以一键加载全部)</span>
         <br/>
-        学校名称：<textarea id="input_sql_control" style="width:460px; height: 60px;"  placeholder="清空不做筛选，多个以英文逗号分隔，或者以换行分隔">北京大学,中国人民大学,清华大学,北京交通大学,北京工业大学,北京航空航天大学,北京理工大学,北京科技大学,北京化工大学,北京邮电大学,中国农业大学,北京林业大学,北京中医药大学,北京师范大学,北京外国语大学,中国传媒大学,中央财经大学,对外经济贸易大学,北京体育大学,中央音乐学院,中央民族大学,中国政法大学,华北电力大学,南开大学,天津大学,天津医科大学,河北工业大学,太原理工大学,内蒙古大学,辽宁大学,大连理工大学,东北大学,大连海事大学,吉林大学,延边大学,东北师范大学,哈尔滨工业大学,哈尔滨工程大学,东北农业大学,东北林业大学,复旦大学,同济大学,上海交通大学,华东理工大学,东华大学,华东师范大学,上海外国语大学,上海财经大学,上海大学,第二军医大学,南京大学,苏州大学,东南大学,南京航空航天大学,南京理工大学,中国矿业大学,河海大学,江南大学,南京农业大学,中国药科大学,南京师范大学,浙江大学,安徽大学,中国科学技术大学,合肥工业大学,厦门大学,福州大学,南昌大学,山东大学,中国海洋大学,中国石油大学,郑州大学,武汉大学,华中科技大学,中国地质大学,武汉理工大学,华中农业大学,华中师范大学,中南财经政法大学,湖南大学,中南大学,湖南师范大学,国防科学技术大学,中山大学,暨南大学,华南理工大学,华南师范大学,广西大学,海南大学,四川大学,西南交通大学,电子科技大学,四川农业大学,西南财经大学,重庆大学,西南大学,贵州大学,云南大学,西藏大学,西北大学,西安交通大学,西北工业大学,西安电子科技大学,长安大学,西北农林科技大学,陕西师范大学,第四军医大学,兰州大学,青海大学,宁夏大学,新疆大学,石河子大学</textarea>
+        学校名称：<textarea id="input_sql_control" style="width:420px; height: 60px;"  placeholder="清空不做筛选，多个以英文逗号分隔，或者以换行分隔">北京大学,中国人民大学,清华大学,北京交通大学,北京工业大学,北京航空航天大学,北京理工大学,北京科技大学,北京化工大学,北京邮电大学,中国农业大学,北京林业大学,北京中医药大学,北京师范大学,北京外国语大学,中国传媒大学,中央财经大学,对外经济贸易大学,北京体育大学,中央音乐学院,中央民族大学,中国政法大学,华北电力大学,南开大学,天津大学,天津医科大学,河北工业大学,太原理工大学,内蒙古大学,辽宁大学,大连理工大学,东北大学,大连海事大学,吉林大学,延边大学,东北师范大学,哈尔滨工业大学,哈尔滨工程大学,东北农业大学,东北林业大学,复旦大学,同济大学,上海交通大学,华东理工大学,东华大学,华东师范大学,上海外国语大学,上海财经大学,上海大学,第二军医大学,南京大学,苏州大学,东南大学,南京航空航天大学,南京理工大学,中国矿业大学,河海大学,江南大学,南京农业大学,中国药科大学,南京师范大学,浙江大学,安徽大学,中国科学技术大学,合肥工业大学,厦门大学,福州大学,南昌大学,山东大学,中国海洋大学,中国石油大学,郑州大学,武汉大学,华中科技大学,中国地质大学,武汉理工大学,华中农业大学,华中师范大学,中南财经政法大学,湖南大学,中南大学,湖南师范大学,国防科学技术大学,中山大学,暨南大学,华南理工大学,华南师范大学,广西大学,海南大学,四川大学,西南交通大学,电子科技大学,四川农业大学,西南财经大学,重庆大学,西南大学,贵州大学,云南大学,西藏大学,西北大学,西安交通大学,西北工业大学,西安电子科技大学,长安大学,西北农林科技大学,陕西师范大学,第四军医大学,兰州大学,青海大学,宁夏大学,新疆大学,石河子大学</textarea>
         <br/>
         年龄小于：<input id="input_2_control" type="text" style="width:40px;margin-right:8px;" value="34" />
         <input id="input_3_control" type="checkbox" checked="checked" style="width: 20px;height: 18px;line-height: 24px;text-align: center;margin: 2px;" />不包含应届生
         <span style="margin-left:8px;">学历：</span>
         <textarea id="input_5_control" style="width:120px; height: 28px;"  placeholder="清空不做筛选，多个以英文逗号分隔，或者以换行分隔">本科,硕士</textarea>
         <br/>
-        求职岗位：<textarea id="input_4_control" style="width:460px; height: 28px;"  placeholder="清空不做筛选，多个以英文逗号分隔，或者以换行分隔">Java,架构师,技术经理,JavaScript,web前端,前端开发,HTML5</textarea>
+        求职岗位：<textarea id="input_4_control" style="width:420px; height: 28px;"  placeholder="清空不做筛选，多个以英文逗号分隔，或者以换行分隔">Java,架构师,技术经理,JavaScript,web前端,前端开发,HTML5</textarea>
         <br/>
         
         <br/>
@@ -33,7 +33,7 @@ panel.innerHTML = `
         <br />
         <br />        
 
-        <div id="result_view" style="width:480px; height: 100px;overflow-y: auto;"></div>
+        <div id="result_view" style="width:420px; height: 100px;overflow-y: auto;"></div>
     </div>
     
 </div>
