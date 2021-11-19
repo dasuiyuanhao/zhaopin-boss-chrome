c5c63d2234231c43b9168e528949eabc435a7418
dasuiyuanhao <dasuiyuanhao@gmail.com>
2021-11-20 00:13:25 +0800

    更新到V1.4版本

--- render.js
+++ render.js
@@ -6,7 +6,7 @@ const panel = document.createElement('div');
 panel.id = 'extend_panel'
 panel.innerHTML = `
 <div class="left">
-    <div style="color:black;background-color: lightblue;cursor: pointer;" title="先设置默认打招呼语，然后打开【推荐牛人】页面，滚动到页面底部加载简历，也可以一键加载全部。已过滤专科学历。">
+    <div style="color:black;background-color: lightblue;cursor: pointer;" title="先设置默认打招呼语，然后打开【推荐牛人】页面，滚动到页面底部加载简历，也可以一键加载全部。">
         <label style="padding-left: 10px;font-size:14px;font-weight: bold;">控制台</label>
         <label style="float:right;padding-right:10px;font-weight: bold;" >?</label>
     </div>   
@@ -14,35 +14,41 @@ panel.innerHTML = `
     <div class="dashboard-edit-box" style="line-height: 30px;">
         <div >            
             <label class="form-label" style="line-height: 52px;">学校名称：</label>
-            <textarea id="input_sql_control" class="form-input" style="width:320px; height: 52px;"  placeholder="清空不做筛选，多个以英文逗号分隔，或者以换行分隔">北京大学,清华大学,中国人民大学,北京师范大学,北京航空航天大学,中国农业大学,北京理工大学,北京科技大学,北京交通大学,中国协和医科大学,中央音乐学院,北京邮电大学,北京外国语大学,北京化工大学,中国政法大学,北京语言大学,北京工业大学中央戏剧学院,中央美术学院,对外经济贸易大学,中央财经大学,北京中医药大学,北京体育大学,北京林业大学,中国传媒大学,国际关系学院,中央民族大学,石油大学,复旦大学,上海交通大学同济大学,华东师范大学,上海财经大学,华东理工大学,上海第二医科大学,上海大学,上海外国语大学,东华大学,南开大学,天津大学,天津医科大学,中央司法警官学院,华北电力大学,燕山大学,河北工业大学,太原理工大学,山西农业大学,内蒙古大学,大连理工大学,东北大学,辽宁大学,大连海事大学,沈阳农业大学,辽宁工程技术大学,吉林大学,东北师范大学,延边大学,哈尔滨工业大学,哈尔滨工程大学,东北林业大学,东北农业大学,大庆石油学院,南京大学,东南大学,中国矿业大学,南京师范大学,南京航空航天大学,南京理工大学,南京农业大学,苏州大学,中国药科大学,河海大学,江苏大学,南京信息工程大学,江南大学,浙江大学,中国科学技术大学,安徽大学合肥工业大学,厦门大学,福州大学,南昌大学,山东大学,中国海洋大学,郑州大学,武汉大学,华中科技大学,华中师范大学,中国地质大学,华中农业大学,武汉理工大学,中南财经政法大学,中南大学,湖南大学,湖南师范大学,湘潭大学,中山大学,华南理工大学,华南师范大学,暨南大学,华南农业大学,广西大学,重庆大学,西南政法大学,西南师范大学,西南农业大学,四川大学,电子科技大学,西南交通大学,西南财经大学,四川农业大学,云南大学,西安交通大学,西北工业大学,西北大学,西安电子科技大学,西北农林科技大学,陕西师范大学,长安大学,陕西科技大学,兰州大学,新疆大学,国防科学技术大学,贵州大学,西北农林科大学,石河子大学,海南大学,宁夏大学,青海大学,西藏大学,第二军医大学,第四军医大学,合肥工业大学西南大学,安徽大学,中国石油大学,同济大学,合肥工业大学,河南大学,国防科技大学,西南大学,上海交通大学,北京工业大学,辽宁大学,东北林业大学,海军军医大学,空军军医大学,成都理工大学,上海中医药大学,天津工业大学,南京邮电大学,首都师范大学,成都中医药大学,宁波大学,上海海洋大学,南京林业大学,天津中医药大学,广州中医药大学,中国科学院大学,南京中医药大学,北京工商大学,延边大学</textarea>
+            <textarea id="input_sql_control" class="form-input" style="width:320px; height: 52px;"  placeholder="清空不做筛选，多个以英文逗号分隔，或者以换行分隔"></textarea>
         </div>   
         <div style="line-height: 30px;height: 30px;margin-top:10px;">
             <label class="form-label" style="line-height: 30px;">年龄小于：</label>
-            <input id="input_2_control" type="text" class="form-input" value="34" style="width:40px;" />
+            <input id="input_2_control" type="text" class="form-input" value="" style="width:40px;" />
 
             <label class="form-label" style="line-height: 30px;margin-left:20px;">学历：</label>
-            <textarea class="form-input" id="input_5_control" style="width:120px; height: 30px;"  placeholder="清空不做筛选，多个以英文逗号分隔，或者以换行分隔">本科,硕士</textarea>
+            <textarea class="form-input" id="input_5_control" style="width:120px; height: 30px;"  placeholder="清空不做筛选，多个以英文逗号分隔，或者以换行分隔"></textarea>
+        
+            <button id="search_btn5_control"  style="min-width: 40px;width: 60px;height: 24px;line-height: 24px;font-size: 10px;margin-left: 20px;" class="form-btn" title="">保存配置</button>
         </div>   
         <div style="line-height: 30px;height: 30px;margin-top:10px;">
             <input class="form-input" id="input_3_control" type="checkbox" checked="checked" style="height: 30px;width: 16px;line-height: 30px;margin: 0;padding: 0;" />
-            <label class="form-label" style="line-height: 30px;">不包含应届生</label>
+            <label class="form-label" style="line-height: 30px;" for="input_3_control">不包含应届生</label>
 
-            <input class="form-input" id="input_6_control" type="checkbox" checked="checked" style="height: 30px;width: 16px;line-height: 30px;margin: 0;padding: 0;margin-left:20px;" />
-            <label class="form-label" style="line-height: 30px;">打招呼后自动滚动到下一个</label>
+            <input class="form-input" id="input_6_control" type="checkbox" style="height: 30px;width: 16px;line-height: 30px;margin: 0;padding: 0;margin-left:20px;" />
+            <label class="form-label" style="line-height: 30px;" for="input_6_control">打招呼后自动滚动到下一个</label>
         </div>        
         
         <div style="line-height: 30px;margin-top:10px;">
             <label class="form-label">求职岗位：</label>
-            <textarea id="input_4_control" class="form-input" style="width:300px; height: 28px;"  placeholder="清空不做筛选，多个以英文逗号分隔，或者以换行分隔">Java,架构师,技术经理,JavaScript,web前端,前端开发,HTML5</textarea>
+            <textarea id="input_4_control" class="form-input" style="width:300px; height: 28px;"  placeholder="清空不做筛选，多个以英文逗号分隔，或者以换行分隔"></textarea>
+        </div>  
+        <div style="line-height: 30px;margin-top:10px;">
+            <label class="form-label">求职意向不包含：</label>
+            <textarea id="input_7_control" class="form-input" style="width:258px; height: 28px;"  placeholder="例如：在职-暂不考虑。多个以英文逗号、换行分隔"></textarea>
         </div>  
         
         <div style="line-height: 30px;margin-top:10px;">
-            <button id="search_btn1_control"  style="width: 150px;height: 30px;margin-right:8px;" class="form-btn">一键加载全部</button>
-            <button id="search_btn2_control"  style="width: 150px;height: 30px;" class="form-btn">筛选全部(Shift+q)</button>
+            <button id="search_btn1_control"  style="width: 130px;height: 30px;margin-right:8px;background-color: orange;" class="form-btn" title="不要使用太频繁，可能会封号">一键加载全部</button>
+            <button id="search_btn2_control"  style="width: 130px;height: 30px;" class="form-btn">筛选全部(Shift+q)</button>
         </div>  
-        <div style="line-height: 30px;margin-top:10px;">
-            <button id="search_btn3_control"  style="width: 150px;height: 30px;margin-right:8px;" class="form-btn">上一个(Shift+a)</button>
-            <button id="search_btn4_control"  style="width: 150px;height: 30px;margin-right:8px;" class="form-btn">下一个(Shift+d)</button>
+        <div style="line-height: 30px;margin-top:10px;height: 40px;">
+            <button id="search_btn3_control"  style="width: 130px;height: 30px;margin-right:8px;" class="form-btn">上一个(Shift+a)</button>
+            <button id="search_btn4_control"  style="width: 130px;height: 30px;margin-right:8px;" class="form-btn">下一个(Shift+d)</button>
             <span id="oneByOne_info_control" style="text-align: center;line-height: 30px;"></span>
         </div>      
 
@@ -171,6 +177,75 @@ $(document).bind('keydown.shift_d', function(){
   selectNext();
 });
 
+/**
+ * 加载默认配置 开始
+ */
+var defaultSetting={
+    filterUniversityName:"",
+};
+
+var loadFromSaved=false;
+if (window.localStorage) {
+    var strOfMySetting = window.localStorage.getItem('mySetting');
+    if(strOfMySetting!=null && strOfMySetting!=""){
+        var mySetting=JSON.parse(strOfMySetting);
+
+        if(mySetting!=null){
+            defaultSetting=mySetting;
+            loadFromSaved=true;
+            printLogInfo("读取配置成功！");
+        }        
+    }
+}
+//如果读取不到本地配置，则设置默认值
+if(!loadFromSaved){
+    //筛选学历 211/985院校
+    // defaultSetting.defaultSetting="北京大学,清华大学,中国人民大学,北京师范大学,北京航空航天大学,中国农业大学,北京理工大学,北京科技大学,北京交通大学,中国协和医科大学,中央音乐学院,北京邮电大学,北京外国语大学,北京化工大学,中国政法大学,北京语言大学,北京工业大学中央戏剧学院,中央美术学院,对外经济贸易大学,中央财经大学,北京中医药大学,北京体育大学,北京林业大学,中国传媒大学,国际关系学院,中央民族大学,石油大学,复旦大学,上海交通大学同济大学,华东师范大学,上海财经大学,华东理工大学,上海第二医科大学,上海大学,上海外国语大学,东华大学,南开大学,天津大学,天津医科大学,中央司法警官学院,华北电力大学,燕山大学,河北工业大学,太原理工大学,山西农业大学,内蒙古大学,大连理工大学,东北大学,辽宁大学,大连海事大学,沈阳农业大学,辽宁工程技术大学,吉林大学,东北师范大学,延边大学,哈尔滨工业大学,哈尔滨工程大学,东北林业大学,东北农业大学,大庆石油学院,南京大学,东南大学,中国矿业大学,南京师范大学,南京航空航天大学,南京理工大学,南京农业大学,苏州大学,中国药科大学,河海大学,江苏大学,南京信息工程大学,江南大学,浙江大学,中国科学技术大学,安徽大学合肥工业大学,厦门大学,福州大学,南昌大学,山东大学,中国海洋大学,郑州大学,武汉大学,华中科技大学,华中师范大学,中国地质大学,华中农业大学,武汉理工大学,中南财经政法大学,中南大学,湖南大学,湖南师范大学,湘潭大学,中山大学,华南理工大学,华南师范大学,暨南大学,华南农业大学,广西大学,重庆大学,西南政法大学,西南师范大学,西南农业大学,四川大学,电子科技大学,西南交通大学,西南财经大学,四川农业大学,云南大学,西安交通大学,西北工业大学,西北大学,西安电子科技大学,西北农林科技大学,陕西师范大学,长安大学,陕西科技大学,兰州大学,新疆大学,国防科学技术大学,贵州大学,西北农林科大学,石河子大学,海南大学,宁夏大学,青海大学,西藏大学,第二军医大学,第四军医大学,合肥工业大学西南大学,安徽大学,中国石油大学,同济大学,合肥工业大学,河南大学,国防科技大学,西南大学,上海交通大学,北京工业大学,辽宁大学,东北林业大学,海军军医大学,空军军医大学,成都理工大学,上海中医药大学,天津工业大学,南京邮电大学,首都师范大学,成都中医药大学,宁波大学,上海海洋大学,南京林业大学,天津中医药大学,广州中医药大学,中国科学院大学,南京中医药大学,北京工商大学,延边大学";
+    //筛选学校 全国一本院校
+    defaultSetting.filterUniversityName="清华大学,北京大学,北京航空航天大学,北京中医药大学,北京语言大学,北京第二外国语学院,北京工商大学,中央民族大学,中国传媒大学,首都师范大学,中国政法大学,中央财经大学,中国人民大学,北京师范大学,中国农业大学,北京外国语大学,北京科技大学,北京邮电大学,北京交通大学,北京化工大学,北京工业大学,北京林业大学,中国地质大学,中国石油大学,中国矿业大学,华北电力大学,北京理工大学,北方工业大学,南开大学,天津大学,天津医科大学,天津财经大学,中国民航大学,河北大学,燕山大学,石家庄铁道大学,山西大学,太原理工大学,中北大学,内蒙古大学,内蒙古工业大学,大连海事大学,辽宁大学,辽宁工程技术大学,大连理工大学,东北大学,沈阳农业大学,中国医科大学,东北财经大学,吉林大学,延边大学,长春理工大学,东北师范大学,哈尔滨理工大学,哈尔滨工程大学,哈尔滨工业大学,黑龙江大学,东北林业大学,东北农业大学,东北石油大学,哈尔滨商业大学,华东理工大学,同济大学,华东师范大学,东华大学,上海外国语大学,上海理工大学,华东政法大学,上海财经大学,上海对外经贸大学,上海大学,上海交通大学,复旦大学,上海海事大学,南京大学,东南大学,南京农业大学,南京工业大学,南京邮电大学,南京财经大学,南京航空航天大学,中国药科大学,河海大学,苏州大学,南京信息工程大学,江苏大学,江苏科技大学,南京理工大学,南京林业大学,南京医科大学,南京中医药大学,南京师范大学,江南大学,浙江大学,宁波诺丁汉大学,宁波大学,浙江工业大学,杭州电子科技大学,浙江理工大学,浙江工商大学,福州大学,福建师范大学,厦门大学,华侨大学,福建农林大学,中国科学技术大学,合肥工业大学,安徽大学,安徽医科大学,安徽师范大学,安徽财经大学,安徽农业大学,安徽工业大学,安徽工程大学,安徽建筑大学,安徽理工大学,南昌大学,华东交通大学,江西财经大学,江西师范大学,南昌航空大学,江西理工大学,江西农业大学,山东大学,山东财经大学,山东农业大学,山东科技大学,青岛大学,中国海洋大学,山东师范大学,山东理工大学,河南大学,郑州大学,华中科技大学,武汉大学,华中师范大学,华中农业大学,武汉理工大学,中南财经政法大学,中南民族大学,长江大学[3],武汉科技大学,湖北工业大学,武汉工程大学,湖北大学,三峡大学,湖南大学,中南大学,湖南师范大学,湖南农业大学,中南林业科技大学,长沙理工大学,湘潭大学,南华大学,湖南科技大学,湖南中医药大学,中山大学,华南师范大学,汕头大学,广州中医药大学,华南理工大学,华南农业大学,南方医科大学,广东外语外贸大学,暨南大学,南方科技大学,广东工业大学,广州大学,深圳大学,广西大学,海南大学,西南大学,西南政法大学,重庆大学,重庆医科大学,重庆交通大学,重庆邮电大学,四川大学,四川农业大学,西南交通大学,电子科技大学,西南财经大学,西南石油大学,西南科技大学,成都理工大学,四川师范大学,成都信息工程大学,贵州大学,云南大学,昆明理工大学,西北工业大学,陕西科技大学,西北农林科技大学,西安交通大学,西安建筑科技大学,西安电子科技大学,西安外国语大学,陕西师范大学,西北大学,长安大学,西安工业大学,西安石油大学,西安理工大学,西安科技大学,兰州大学,青海大学,宁夏大学,新疆大学,石河子大学";
+    //年龄小于
+    defaultSetting.ageLessThan= "34";
+    //学历
+    defaultSetting.filterEducation = "本科,硕士";
+    //不包含应届生
+    defaultSetting.filterYingJie=true;
+    //打招呼后自动滚动到下一个
+    defaultSetting.autoScrollNext=false;
+    //求职岗位
+    defaultSetting.filterExpects="Java,架构师,技术经理,JavaScript,web前端,前端开发,HTML5";
+    //求职意向不包含
+    defaultSetting.filterWillNotIn="";
+}
+
+//学校名称
+$("#input_sql_control").val(defaultSetting.filterUniversityName);
+//年龄小于
+$("#input_2_control").val(defaultSetting.ageLessThan);
+//学历
+$("#input_5_control").val(defaultSetting.filterEducation);
+//不包含应届生
+if(defaultSetting.filterYingJie){
+    $("#input_3_control").attr("checked","checked");
+}
+else{
+    $("#input_3_control").attr("checked",false);
+}
+//打招呼后自动滚动到下一个
+if(defaultSetting.autoScrollNext){
+    $("#input_6_control").attr("checked","checked");
+}
+else{
+    $("#input_6_control").attr("checked",false);
+}
+//求职岗位
+$("#input_4_control").val(defaultSetting.filterExpects);
+//求职意向不包含
+$("#input_7_control").val(defaultSetting.filterWillNotIn);
+
+/**
+ * 加载默认配置 结束
+ */
+
 //绑定按钮事件
 const btnSC1=document.getElementById("search_btn1_control");
 btnSC1.addEventListener('click', e=>{    
@@ -199,6 +274,51 @@ btnSC3.addEventListener('click', e=>{
 const btnSC4=document.getElementById("search_btn4_control");
 btnSC4.addEventListener('click', e=>{
     selectNext();
+});
+
+//保存配置
+const btnSC5=document.getElementById("search_btn5_control");
+btnSC5.addEventListener('click', e=>{    
+    try {
+        if (window.localStorage) {
+            //学校名称
+            var strFilterUniversityName = $.trim($("#input_sql_control").val());
+            defaultSetting.filterUniversityName=strFilterUniversityName;
+            //年龄小于
+            defaultSetting.ageLessThan= $.trim($("#input_2_control").val());
+            //学历
+            defaultSetting.filterEducation = $.trim($("#input_5_control").val());
+            //不包含应届生
+            var filterYingJie=false;
+            if($("#input_3_control").prop("checked")){
+                filterYingJie=true;
+            }
+            defaultSetting.filterYingJie=filterYingJie;
+            //打招呼后自动滚动到下一个
+            var autoScrollNext=false;
+            if($("#input_6_control").prop("checked")){
+                autoScrollNext=true;
+            }
+            defaultSetting.autoScrollNext=autoScrollNext;
+            //求职岗位
+            defaultSetting.filterExpects=$.trim($("#input_4_control").val());
+            //求职意向不包含
+            defaultSetting.filterWillNotIn=$.trim($("#input_7_control").val());
+
+            var strOfMySetting=JSON.stringify(defaultSetting);
+            if(strOfMySetting==null || strOfMySetting==""){
+                printLogInfo("配置参数不能为空");
+                return;
+            }
+
+            window.localStorage.setItem('mySetting',strOfMySetting);
+            printLogInfo("保存配置成功！");
+        }
+    } catch (error) {
+        printLogInfo("保存异常！请检查配置");
+        console.error(error);
+        return;
+    }
 });
 
 /**滚动加载全部 */
@@ -290,7 +410,10 @@ function scrollLoadRecommendEnd(){
 
         if(thisTimeLast==scollLoadEndItem){
             tryScrollLoadFailedTimes++;
-            return scrollLoadRecommendEnd();
+            setTimeout(function(){
+                return scrollLoadRecommendEnd();
+            }, randomNum(2000,10000));
+            return;
         }
 
         scollLoadEndItem=thisTimeLast;
@@ -313,7 +436,7 @@ function scrollLoadRecommendEnd(){
     setTimeout(function(){
         return scrollLoadRecommendEnd();       
 
-    }, 2000);    
+    }, randomNum(2000,10000));
 }
 
 function doThings(){
@@ -383,6 +506,18 @@ function doThings(){
         }
         filterEducations=trimArray(filterEducations);
     }
+    //求职意向
+    var filterWillNotIn=[];
+    var input_7_val=$.trim($("#input_7_control").val());
+    if(input_7_val!=''){
+        //如果是带换行符
+        input_7_val=input_7_val.replace(/\n/g,",");
+        filterWillNotIn=input_7_val.split(",");
+        if(filterWillNotIn==null){
+            filterWillNotIn=[];
+        }
+        filterWillNotIn=trimArray(filterWillNotIn);
+    }    
         
     // isExcuting=true;
 
@@ -555,6 +690,23 @@ function doThings(){
                 }
                 if(!eduFilter){
                     checkResult+="学历不符合条件。";
+                }
+            }
+        }
+
+        //过滤求职意向
+        if(filterWillNotIn!=null && filterWillNotIn.length>0){
+            if(item.jobStatus){
+                var enableFilterWill=true;
+                for(var j=0;j<filterWillNotIn.length;j++){
+                    var itemFWN=filterWillNotIn[j];
+                    if(item.jobStatus==itemFWN){
+                        enableFilterWill=false;
+                        break;
+                    }
+                }
+                if(!enableFilterWill){
+                    checkResult+="求职意向不符合条件。";
                 }
             }
         }
@@ -784,3 +936,20 @@ function btnCommunicateRefresh(e){
     }
     
 }
+
+/**
+ * 生成指定范围内的随机整数
+ */
+function randomNum(minNum,maxNum){  
+    switch(arguments.length){ 
+        case 1: 
+            return parseInt(Math.random()*minNum+1); 
+        break;
+        case 2: 
+            return parseInt(Math.random()*(maxNum-minNum+1)+minNum); 
+        break;
+        default: 
+            return 0; 
+        break;    
+    } 
+} 
